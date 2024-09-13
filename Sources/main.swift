582805b0b60e0d02ee40089e208187421d2eedc5
Cameron Clough <cameronjclough@gmail.com>
2024-09-13 23:53:26 +0100

    feat: read challenge file

--- Sources/main.swift
+++ Sources/main.swift
@@ -11,4 +11,43 @@
 // - ???
 // - Profit
 
-print("Hello, world!")
+import Foundation
+
+struct FileLoader {
+    enum Error: Swift.Error {
+        case fileNotFound(path: String)
+        case failedToLoadData(path: String, Swift.Error)
+    }
+
+    static func load(path: String) throws -> String {
+        let url = URL(fileURLWithPath: path)
+        do {
+            let data = try Data(contentsOf: url)
+            let html = String(data: data, encoding: .utf8)
+            if html == nil {
+                throw Error.fileNotFound(path: path)
+            }
+            return html!
+        } catch {
+            throw Error.failedToLoadData(path: path, error)
+        }
+    }
+}
+
+struct AppArguments {
+    var path: String
+
+    init() {
+        let defaults = UserDefaults.standard
+        self.path = defaults.string(forKey: "path") ?? "challenge.html"
+    }
+}
+
+let args = AppArguments()
+
+do {
+    let html = try FileLoader.load(path: args.path)
+    print(html)
+} catch {
+    print("Error: \(error)")
+}
