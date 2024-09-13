5460fa16af69a3b1560c9c2b71e685bb91315bd8
Cameron Clough <cameronjclough@gmail.com>
2024-09-13 20:01:01 +0100

    chore: editorconfig

--- Sources/main.swift
+++ Sources/main.swift
@@ -2,3 +2,16 @@
 // https://docs.swift.org/swift-book
 
 print("Hello, world!")
+
+let constant = 42
+var variable = 42
+variable = 50
+
+print(constant)
+print(variable)
+
+if constant == variable {
+    print("constant and variable are equal")
+} else {
+    print("constant and variable are not equal")
+}
